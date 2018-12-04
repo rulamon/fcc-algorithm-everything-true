250336eedbcd7bf294dbcb8512f430a3c158f85b
rulamon <ruben.lamon@gmail.com>
2018-12-04 22:10:47 +0100

    solution after hints using .every

---
+++ 2.js
@@ -0,0 +1,4 @@
+function truthCheck(collection, pre) {
+	return collection.every(val => Boolean(val[pre]))
+}
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"))
