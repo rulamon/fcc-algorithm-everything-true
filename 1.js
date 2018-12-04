b36c5574d25b8686a835dfaa8f9df7ff9e318478
rulamon <ruben.lamon@gmail.com>
2018-12-04 21:44:42 +0100

    first solution

---
+++ 1.js
@@ -0,0 +1,10 @@
+function truthCheck(collection, pre) {
+	for (let i = 0; i < collection.length; i++) {
+		if (!Boolean(collection[i][pre])) {
+			return false
+		}
+	}
+  return true;
+}
+
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"))
